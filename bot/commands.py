16b127c853167c5a286d828e82a858cf95535450
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2024-07-16 11:54:19 +0530

    Update commands.py

--- bot/commands.py
+++ bot/commands.py
@@ -7,40 +7,41 @@ from .constants import START_TEXT,HELP_TEXT,ABOUT_TEXT
 
 @Client.on_message(filters.private & filters.command(["start"]))
 async def start(bot, message, cb=False):
+    if cb:
+        message = message.message
     await add_user(message.from_user.id)
-    # something here
+    await message.reply_text(
+        text=START_TEXT,
+        reply_markup=START_BUTTONS,
+        disable_web_page_preview=True,
+        quote=True,
+    )
 
 @Client.on_message(filters.private & filters.command(["help"]))
 async def help(bot, message, cb=False):
-    await add_user(message.from_user.id)
     if cb:
-        await message.message.edit_text(
-            text=HELP_TEXT,
-            reply_markup=HELP_BUTTONS,
-            disable_web_page_preview=True
-        )
-    else:
-        await message.reply_text(
-            text=HELP_TEXT,
-            reply_markup=HELP_BUTTONS,
-            disable_web_page_preview=True,
-            quote=True
-        )
+        message = message.message
+    await add_user(message.from_user.id)
+    await message.reply_text(
+        text=HELP_TEXT,
+        reply_markup=HELP_BUTTONS,
+        disable_web_page_preview=True,
+        quote=True,
+    )
+
 
 @Client.on_message(filters.private & filters.command(["about"]))
 async def about(bot, message, cb=False):
-    await add_user(message.from_user.id)
-    text = ABOUT_TEXT.format((await bot.get_me()).username)
     if cb:
-        await message.message.edit_text(
-            text=text,
-            reply_markup=ABOUT_BUTTONS,
-            disable_web_page_preview=True
-        )
-    else:
-        await message.reply_text(
-            text=text,
-            reply_markup=ABOUT_BUTTONS,
-            disable_web_page_preview=True,
-            quote=True
-        )
+        message = message.message
+    await add_user(message.from_user.id)
+    await message.reply_text(
+        text=ABOUT_TEXT,
+        reply_markup=ABOUT_BUTTONS,
+        disable_web_page_preview=True,
+        quote=True,
+    )
+
+@Client.on_message(filters.private & filters.command(["command"]))
+async def func(bot, message: Message):
+    pass
