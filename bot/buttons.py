0d262cff8c936bb659ffb0780756fd863535e76b
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2024-08-04 13:22:49 +0530

    refactor :Update buttons.py

--- bot/buttons.py
+++ bot/buttons.py
@@ -1,4 +1,4 @@
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from hydrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 help_button = InlineKeyboardButton('⚙ Help', callback_data='help')
@@ -9,19 +9,19 @@ home_button = InlineKeyboardButton('🏘 Home', callback_data='home')
 
 START_BUTTONS = InlineKeyboardMarkup(
     [
-        [help_button, about_button, close_button]
+        [help_button, about_button],[close_button]
     ]
 )
 
 HELP_BUTTONS = InlineKeyboardMarkup(
     [
-        [home_button, about_button, close_button]
+        [home_button, about_button],[close_button]
     ]
 )
 
 ABOUT_BUTTONS = InlineKeyboardMarkup(
     [
-        [home_button, help_button, close_button]
+        [home_button, help_button],[close_button]
     ]
 )
 
