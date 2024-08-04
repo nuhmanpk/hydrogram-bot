8642e2f2b55d51b0e54b485b492df4cbc62916b2
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2024-08-04 13:22:04 +0530

    fix : better implementation - Update callbacks.py

--- bot/callbacks.py
+++ bot/callbacks.py
@@ -2,17 +2,48 @@ from .database import db
 from .admin import add_user
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
-from .commands import start, help, about
+from .constants import (
+    START_TEXT,
+    HELP_TEXT,
+    ABOUT_TEXT
+)
+from .buttons import (
+    HELP_BUTTONS, 
+    START_BUTTONS, 
+    ABOUT_BUTTONS,
+    CLOSE_BUTTON
+)
 
 
 @Client.on_callback_query()
 async def cb_data(_, message):
     await add_user(message.from_user.id)
     if message.data == "home":
-        await start(_, message, cb=True)
+        await message.message.delete()
+        message = message.message
+        await message.reply_text(
+        text=START_TEXT,
+        reply_markup=START_BUTTONS,
+        disable_web_page_preview=True,
+        quote=True,
+        )
     elif message.data == "help":
-        await help(_, message, cb=True)
+        await message.message.delete()
+        message = message.message
+        await message.reply_text(
+        text=HELP_TEXT,
+        reply_markup=HELP_BUTTONS,
+        disable_web_page_preview=True,
+        quote=True,
+        )
     elif message.data == "about":
-        await about(_, message, cb=True)
+        await message.message.delete()
+        message = message.message
+        await message.reply_text(
+        text=ABOUT_TEXT,
+        reply_markup=ABOUT_BUTTONS,
+        disable_web_page_preview=True,
+        quote=True,
+        )
     elif message.data == "close":
         await message.message.delete()
